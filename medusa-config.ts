81ee01e3cf1b741bdf999322b094e3fcbca30b6b
unknown <mihidebi@gmail.com>
2025-11-11 15:55:38 +0530

    Add Redis configuration to medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -16,6 +17,24 @@ module.exports = defineConfig({
   modules: [
     {
       resolve: "./src/modules/subscription",
+    },
+    {
+      resolve: "@medusajs/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/locking-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
     },
   ],
 })
